ab981a3ac82f2a09ed24f68f2298c60f8cd2f421
ryadav29 <rajat.yadav@vvdntech.in>
2024-03-17 22:05:32 +0530

    ClumpCoder_1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './Login';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+  <div className="g-signin">
+    <Login />
+    
+  </div>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
